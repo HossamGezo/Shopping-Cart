cf6ff02baaac0437224728c512030a317d14cd7b
HossamGezo <ha2ghossam10@gmail.com>
2023-12-12 09:01:27 +0200

    Edit Path For Index.html To Work With GitHub Domain

--- assets/js/main.js
+++ assets/js/main.js
@@ -218,7 +218,7 @@ if (!localStorage.user) localStorage.setItem("user", JSON.stringify(user));
 
 setupUI();
 
-if (window.location.pathname === "/index.html") {
+if (window.location.pathname === "/index.html" || window.location.pathname === "/Shopping-Cart/") {
   showProducts();
 }
 // ! ------------------- Events 
@@ -379,7 +379,7 @@ mainContent.addEventListener("click", function (event) {
 });
 
 // Search Input Event
-if (window.location.pathname === "/index.html") {
+if (window.location.pathname === "/index.html" || window.location.pathname === "/Shopping-Cart/") {
   search.addEventListener("keyup", function () {
     let itSolid;
     if (localStorage.currentUser) {
@@ -669,7 +669,7 @@ function showProducts(array = []) {
   });
   allProducts.innerHTML = products.join("");
 }
-if (window.location.pathname === "/index.html") {
+if (window.location.pathname === "/index.html" || window.location.pathname === "/Shopping-Cart/") {
   showProducts();
 }
 
